9a5d072acb5d173856d6aa6b90cafdda1762686a
Jardson Almeida da Rosa <jardson.rosa@outlook.com>
2020-10-19 20:10:26 -0300

    bytebank project

---
+++ index.js
@@ -0,0 +1,14 @@
+import { Cliente } from "./Cliente.js"
+import { ContaCorrente } from "./ContaCorrente.js"
+
+const cliente1 = new Cliente("Ricardo", 11122233309);
+const cliente2 = new Cliente("Alice", 88822233309);
+
+const contaCorrenteRicardo = new ContaCorrente(cliente1, 1001);
+contaCorrenteRicardo.depositar(500);
+
+const conta2 = new ContaCorrente(cliente2, 1002);
+
+console.log(conta2);
+console.log(contaCorrenteRicardo);
+console.log(ContaCorrente.numeroDeContas);
